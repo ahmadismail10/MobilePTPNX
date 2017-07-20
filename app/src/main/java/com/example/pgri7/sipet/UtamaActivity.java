d112e81e3b24b40dedf8722dacb73d3c4abfd3d1
Muhsin <muhsin21baf@hotmail.com>
2017-07-20 15:24:09 +0700

    new version

--- app/src/main/java/com/example/pgri7/sipet/UtamaActivity.java
+++
@@ -1,8 +0,0 @@
-package com.example.pgri7.sipet;
-
-/**
- * Created by PGRI7 on 14/07/2017.
- */
-
-public class UtamaActivity {
-}
---
+++ app/src/main/java/info/androidhive/loginandregistration/activity/MainActivity.java
@@ -0,0 +1,78 @@
+package info.androidhive.loginandregistration.activity;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+import java.util.HashMap;
+
+import info.androidhive.loginandregistration.R;
+import info.androidhive.loginandregistration.helper.SQLiteHandler;
+import info.androidhive.loginandregistration.helper.SessionManager;
+
+public class MainActivity extends Activity {
+
+	private TextView txtName;
+	//private TextView txtUsername;
+	private Button btnLogout;
+
+	private SQLiteHandler db;
+	private SessionManager session;
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_main);
+
+		txtName = (TextView) findViewById(R.id.name);
+		//txtUsername = (TextView) findViewById(R.id.username);
+		btnLogout = (Button) findViewById(R.id.btnLogout);
+
+		// SqLite database handler
+		db = new SQLiteHandler(getApplicationContext());
+
+		// session manager
+		session = new SessionManager(getApplicationContext());
+
+		if (!session.isLoggedIn()) {
+			logoutUser();
+		}
+
+		// Fetching user details from SQLite
+		HashMap<String, String> user = db.getUserDetails();
+
+		String name = user.get("name");
+		//String username = user.get("username");
+
+		// Displaying the user details on the screen
+		txtName.setText(name);
+		//txtUsername.setText(username);
+
+		// Logout button click event
+		btnLogout.setOnClickListener(new View.OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				logoutUser();
+			}
+		});
+	}
+
+	/**
+	 * Logging out the user. Will set isLoggedIn flag to false in shared
+	 * preferences Clears the user data from sqlite users table
+	 * */
+	private void logoutUser() {
+		session.setLogin(false);
+
+		db.deleteUsers();
+
+		// Launching the login activity
+		Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+		startActivity(intent);
+		finish();
+	}
+}
